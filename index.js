d33b02cfeb22849f3a44cc27a501d625f0aa411e
Mohit Tekwani <83222773+tekwaniMohit@users.noreply.github.com>
2021-07-03 11:36:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+$("h1").css("font-size", "5rem");
+
+$("button").click(function(){
+    $("h1").css("color" , "grey")
+});
+
+
+
+$("button").css("font-size","3rem");
+
+
+$("button").click(function() {
+ var audio = new Audio('audio/right_here_waiting.mp3');
+ audio.play()
+
+});
